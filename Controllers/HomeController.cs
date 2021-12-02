88e8c28d8ff01605654d4c233e15e20b62f200f4
Dmitriy Stupin <dmitriy-stupin@mail.ru>
2021-12-02 21:07:43 +0300

    Add IEnumerable2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,16 +8,56 @@ namespace LanguagesFeatures.Contrillers
     {
         public ViewResult Index()
         {   
-            List<string> results = new List<string>();
-            foreach (Product p in Product.GetProducts())
-            {
-                string name = p?.Name ?? "<Noname>";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name ?? "<None>";
-                string category = p?.Category ?? "<No category>";
-                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}, Category: {category}");
-            }
-            return View(results);
+
+            ShoppingCart cart = new ShoppingCart {Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+            return View ("Index", cartTotal);
+        //     List<string> results = new List<string>();
+        //     foreach (Product p in Product.GetProducts())
+        //     {
+        //         string name = p?.Name ?? "<Noname>";
+        //         decimal? price = p?.Price ?? 0;
+        //         string relatedName = p?.Related?.Name ?? "<None>";
+        //         string category = p?.Category ?? "<No category>";
+                
+        //         results.Add($"Name: {name}, Price: {price}, Related: {relatedName}, Category: {category}, Instock: {stock}");
+        //     }
+        //     return View(results);
+        // }
+        // Dictionary<string, Product> products = new Dictionary<string, Product>{
+        //     {"Kayak", new Product {Name = "Kayak", Price = 258M}},
+        //     {"LifeJacket", new Product {Name = "LifeJacket", Price = 48.95M}}
+        //     };
+        //     return View("Index", products.Keys);
+            // Dictionary<string, Product> products = new Dictionary<string, Product>{
+            // ["Kayak"] = new Product {Name = "Kayak", Price = 258M},
+            // ["LifeJacket"] = new Product {Name = "LifeJacket", Price = 48.95M},
+            // };
+            // return View("Index", products.Keys);
+        // object[] data = new object [] {275M, 29.95M, "apple", "orange", 100, 10};
+        // decimal total = 0;
+        // for (int i = 0; i < data.Length; i++)
+        // {
+        //     if(data[i] is decimal d)
+        //     {total += d;}
+        // }
+         
+        //     for (int i = 0; i < data.Length; i++){
+
+        //         switch (data[i]){
+        //           case decimal DecimalValue:
+        //                 total += DecimalValue;
+        //                 break;
+        //             case int intValue when intValue > 50:
+        //                 total += intValue;
+        //                 break;
+                
+        //         }
+        //     }
+        
+        // return View("Index", new string[] {$"Total: {total:C2}"});
+
+
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,7 +1,11 @@
 namespace LanguageFeatures.Models
 {
-    public class Product
-    {
+    public class Product 
+    {   
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
         public string Name {get; set;}
         public decimal? Price {get; set;}
         public Product Related {set; get;}
@@ -9,7 +13,7 @@ namespace LanguageFeatures.Models
         public bool InStock {get;} = true;
         public static Product[] GetProducts() {
             Product kayak = new Product {Name = "Kayak", Price = 275M, Category = "Water Craft"};
-            Product lifejacket = new Product {Name = "Lifejacket", Price = 48.95M};
+            Product lifejacket = new Product(false) {Name = "Lifejacket", Price = 48.95M};
             kayak.Related = lifejacket;
             return new Product[] {kayak, lifejacket, null};
         }
